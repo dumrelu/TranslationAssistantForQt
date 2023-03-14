30efcd23d59328385bfec2f1cb2fff33f2b355da
Relu Dumitru <dum.relu@gmail.com>
2023-03-14 20:01:36 +0200

    Added a simple test .ts file

--- test/TranslationFilesLoadTest.h
+++ test/TranslationFilesLoadTest.h
@@ -6,10 +6,23 @@ class TranslationFilesLoadTest : public QObject
 {
     Q_OBJECT
 public:
+    TranslationFilesLoadTest(QString executableDir)
+        : m_executableDir{ std::move(executableDir) }
+    {
+        qDebug() << "Executable dir" << m_executableDir;
+    }
 
 private slots:
+    void initTestCase()
+    {
+        QVERIFY(QFile::exists(m_executableDir + "/simple_file.ts"));
+    }
+
     void testHelloWorld()
     {
         QVERIFY(false);
     }
+
+private:
+    QString m_executableDir;
 };
--- test/test_main.cpp
+++ test/test_main.cpp
@@ -1,13 +1,25 @@
 #include "TranslationFilesLoadTest.h"
 
+#include <QFile>
+
+template <typename T, class... Params>
+void execute_test(int argc, char* argv[], Params&&... params)
+{
+    T test{ std::forward<Params>(params)... };
+    QTest::qExec(&test, argc, argv);
+}
+
 template <typename T>
 void execute_test(int argc, char* argv[])
 {
-    T test;
+    T test{};
     QTest::qExec(&test, argc, argv);
 }
 
 int main(int argc, char* argv[])
 {
-    execute_test<TranslationFilesLoadTest>(argc, argv); 
+    QFileInfo fileInfo{ QString{ argv[0] } };
+    QString executableDir = fileInfo.absoluteDir().absolutePath();
+
+    execute_test<TranslationFilesLoadTest>(argc, argv, executableDir); 
 }
