23493bbd2820f34347f760ce1e27b9cd5dc6742a
Relu Dumitru <dum.relu@gmail.com>
2023-04-30 20:23:54 +0300

    Retrieve context for item

--- src/translationassistant.cpp
+++ src/translationassistant.cpp
@@ -1,9 +1,34 @@
 #include "translationassistant.h"
 
+#include <QQmlEngine>
+#include <QQmlContext>
 #include <QDebug>
 
 namespace ta
 {
+
+namespace
+{
+
+// Function that returns the QQmlContext for a QQuickItem
+QQmlContext* qmlContextForItem(QQuickItem* item)
+{
+    QQmlContext* context = QQmlEngine::contextForObject(item);
+    if(context)
+    {
+        return context;
+    }
+
+    QQuickItem* parent = item->parentItem();
+    if(parent)
+    {
+        return qmlContextForItem(parent);
+    }
+
+    return nullptr;
+}
+
+}
 
 TranslationAssistant::TranslationAssistant(QQuickWindow *window, QObject *parent)
     : QObject{ parent }
@@ -43,6 +68,13 @@ void TranslationAssistant::onTextItemInvalidated(QSharedPointer<TextItem> textIt
 void TranslationAssistant::onTextItemClicked(QSharedPointer<TextItem> textItem)
 {
     qDebug() << "Clicked on item: " << textItem->text();
+
+    auto* context = qmlContextForItem(textItem->item());
+    if(context)
+    {
+        qDebug() << "Context: " << context->baseUrl();
+        return;
+    }
 
     auto it = m_textItemOverlays.find(textItem);
     if(it != m_textItemOverlays.end())
